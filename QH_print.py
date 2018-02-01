63c4c465aaaa82923ee44b498a43efee47ca121b
Uxer@Joanwe <joanwelol@gmail.com>
2018-02-01 17:38:27 +0800

    _

--- QH_print.py
+++ QH_print.py
@@ -15,14 +15,15 @@ table.align['答案选项'] = 'l'
 def print_table(value):
     value = json.loads(value)
     title = value['title']
-    letter = ''
-    letters = ['1.', '2.', '3.']
     recommend = value['recommend']
     result = value['result']
     summary = value['search_infos'][0]['summary']
+    dic = {}
+    sum = 1
     for i in value['answers']:
         if recommend or result in i:
-            letter = letters[value['answers'].index(i)]
+            dic[i] = sum
+            sum += 1
     for i in value['answers']:
         if i in summary:
             # 打印绿色高亮字体    '\033[0;32m' +  + '\033[0m'
@@ -31,8 +32,9 @@ def print_table(value):
     print(title)
     print(table)
     if value['recommend'] == '啊呀，这题汪仔还在想':
-        print('推荐答案:', letter + result)
+        print('推荐答案:', str(dic[result]) + '.' + result)
+
     else:
-        print('推荐答案:', letter + recommend)
+        print('推荐答案:', str(dic[recommend]) + '.' + recommend)
     print('参考:', summary)
     print('\n')
